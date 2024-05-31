a941d42415c54eebf05541dc6d6caf4ce322b0c7
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-05-31 20:19:14 -0300

    projeto atualizado

--- js/imc-script.js
+++ js/imc-script.js
@@ -1,16 +1,21 @@
 const inputWeight = document.getElementById("ipeso");
 const inputHeight = document.getElementById("ialtura");
+const spanHeight = document.getElementById("span-height");
+const spanWeight = document.getElementById("span-weight");
 
 
-
-function discoverError(input)
+function discoverError(input, span)
 {
     input.style.outline = `2px solid red`;
+    span.style.color = `red`;
+    span.innerText = `Erro valor não condiz com o campo!!`;
 }
 
-function inputValid(input)
+function inputValid(input, span)
 {
     input.style.outline = `2px solid #A5E381`;
+    span.style.color = `#A5E381`;
+    span.innerText = `Kg`;
 }
 
 function nullInput(input)
@@ -23,18 +28,42 @@ function verifyValueHeight()
     
     let numHeight = Number(inputHeight.value);
     
-    if(inputHeight.value.length == "") nullInput(inputHeight);
-    else if(numHeight > 2.51 || numHeight < 1.30) discoverError(inputHeight);
-    else inputValid(inputHeight)
-
+    if(inputHeight.value.length == "") 
+    {
+        nullInput(inputHeight);
+        spanHeight.classList.remove("height-span")
+    }
+    else if((numHeight < 1.30 || numHeight > 2.51) || inputHeight.value.length != 4)
+    {
+        discoverError(inputHeight, spanHeight);
+        spanHeight.classList.add("height-span")
+    }
+    else
+    { 
+        inputValid(inputHeight, spanHeight)
+        spanHeight.classList.add("height-span")
+    }
 }
 
 function verifyValueWeight()
 {
     let numWeight = Number(inputWeight.value);
-    if(inputWeight.value.length == "") nullInput(inputWeight);
-    else if(numWeight > 200 || numWeight < 20) discoverError(inputWeight);
-    else inputValid(inputWeight)
+    if(inputWeight.value.length == "") 
+    {
+        nullInput(inputWeight);
+        spanWeight.classList.remove("weight-span")
+    }
+    else if(numWeight > 200 || numWeight < 20)
+    {
+        discoverError(inputWeight, spanWeight);
+        spanWeight.classList.add("weight-span")
+
+    }
+    else
+    {
+        inputValid(inputWeight, spanWeight)
+        spanWeight.classList.add("weight-span")
+    }
 }
 
 inputHeight.addEventListener("input", verifyValueHeight);
