82ad6fa8b717981d8508946f3aca62646fedc10f
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-05-31 23:36:08 -0300

    projeto academia atualizado

--- js/imc-script.js
+++ js/imc-script.js
@@ -2,8 +2,7 @@ const inputWeight = document.getElementById("ipeso");
 const inputHeight = document.getElementById("ialtura");
 const spanHeight = document.getElementById("span-height");
 const spanWeight = document.getElementById("span-weight");
-
-
+const buttonIMC = document.getElementById("button-imc");
 function discoverError(input, span)
 {
     input.style.outline = `2px solid red`;
@@ -23,27 +22,6 @@ function nullInput(input)
     input.style.outline = `none`;
 }
 
-function verifyValueHeight()
-{
-    
-    let numHeight = Number(inputHeight.value);
-    
-    if(inputHeight.value.length == "") 
-    {
-        nullInput(inputHeight);
-        spanHeight.classList.remove("height-span")
-    }
-    else if((numHeight < 1.30 || numHeight > 2.51) || inputHeight.value.length != 4)
-    {
-        discoverError(inputHeight, spanHeight);
-        spanHeight.classList.add("height-span")
-    }
-    else
-    { 
-        inputValid(inputHeight, spanHeight)
-        spanHeight.classList.add("height-span")
-    }
-}
 
 function verifyValueWeight()
 {
@@ -66,6 +44,60 @@ function verifyValueWeight()
     }
 }
 
+function verifyValueHeight()
+{
+    
+    let numHeight = Number(inputHeight.value);
+    
+    if(inputHeight.value.length == "") 
+    {
+        nullInput(inputHeight);
+        spanHeight.classList.remove("height-span");
+    }
+    else if((numHeight < 1.30 || numHeight > 2.51) || inputHeight.value.length != 4)
+    {
+        discoverError(inputHeight, spanHeight);
+        spanHeight.classList.add("height-span");
+    }
+    else
+    { 
+        inputValid(inputHeight, spanHeight);
+        spanHeight.classList.add("height-span");
+    }
+}
+
+function discoverIMC()
+{
+    let numWeight = Number(inputWeight.value);
+    let numHeight = Number(inputHeight.value);
+    let currentIMC = numWeight / (numHeight**2)
+    return currentIMC.toFixed(1);
+}
+
+function outputIMC()
+{
+    if(discoverIMC() <= 18.5)
+    {
+        containerOutput.innerHTML = `Abaixo do Peso`;
+    }
+    else if(discoverIMC() >= 18.6 && discoverIMC() <= 24.9)
+    {
+        containerOutput.innerHTML = `Peso Ideal!! Continue na pegada`;
+    }
+    else if(discoverIMC() >= 25 && discoverIMC() <= 29.9)
+    {
+        containerOutput.innerHTML = `Sobrepeso recomendamos que comece a se cuidar`;
+    }
+    else if(discoverIMC() >= 30 && discoverIMC() <= 34.9)
+    {
+        containerOutput.innerHTML = `Obesidade grau I`;
+    }
+    else if(discoverIMC() >= 35)
+    {
+        
+    }
+}
+
 inputHeight.addEventListener("input", verifyValueHeight);
-inputHeight.onkeypress = () => {if(inputHeight.value.length == 4) return false;}
 inputWeight.addEventListener("input", verifyValueWeight);
+buttonIMC.addEventListener("click", outputIMC);
